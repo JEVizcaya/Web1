aa1a81c7fbbc1451cd82ff6bcf87a39027d1bacd
JEVizcaya <jevizcaya@hotmail.com>
2025-03-10 20:02:54 +0100

    conectando a bbdd

--- app.py
+++ app.py
@@ -1,43 +1,66 @@
-from flask import Flask, render_template, request,redirect, url_for,session
+from flask import Flask,render_template,request,redirect,url_for,session
+import pymysql
+
+
+
 
 app = Flask(__name__)
-app.secret_key='123456'
+app.secret_key="123456"
 
-@app.route('/login', methods=['GET'])
+@app.route('/login',methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
+#recibir dos parametros nombre y edad
 @app.route('/saludo/<nombre>/<int:edad>')
-def saludo(nombre,edad):
-    if edad < 18:
-        return f'Hola, {nombre} Eres menor de edad.'
+def prueba(nombre,edad):
+    if(edad<18):
+        return "Hola "+nombre+" eres menor de edad"
     else:
-        
-        return f'Hola, {nombre} Eres mayor de edad.'
-    
+        return "Hola "+nombre+" eres mayor de edad"
+
 #login post
-
-@app.route('/login', methods=['POST'])
+@app.route('/login',methods=['POST'])
 def login():
-   #obtener datos formulario
-    username = request.form['username']
+    #obtener los datos del formulario
+    username = request.form['username'] 
     password = request.form['password']
-    if (username == 'admin' and password == 'admin'):
-        #guardar en sesion
-        session['username'] = username
-        
-        return redirect(url_for('admin'))
-    else:
+    #creamos la conexion
+    conexion = pymysql.connect(
+        host='localhost',
+        user='root', 
+        password='', 
+        db='sakila')
+    try:
+        with conexion.cursor() as cursor:
+            #creamos la consulta
+            consulta = '''SELECT * FROM usuario 
+            WHERE username = %s AND password = %s'''
+        datos = (username,password)
+        cursor.execute(consulta,datos)
+        resultados = cursor.fetchone()
+        if(len(resultados)>0):
+            #guardar datos en session
+            session['username'] = username
+            return redirect(url_for('admin'))
+        else:
+            return render_template("index.html",mensaje="Usuario o contraseña incorrecta")
+    except Exception as e:
+        print("Ocurrió un error al consultar: ", e)
+    finally:    
+        conexion.close()
+        print("Conexión cerrada") 
+          
     
-        return render_template('index.html', error='Usuario o contraseña incorrectos')
-
-@app.route('/admin', methods=['GET'])
+    
+    
+@app.route('/admin',methods=['GET'])
 def admin():
-    if 'username' in session:
-        return render_template('admin/admin.html')
+    if "username" in session:
+        return render_template("admin/admin.html")
     else:
         return redirect(url_for('login'))
-   
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     app.run(debug=True,port=80)
+    
--- pruebasconexion.py
+++ pruebasconexion.py
@@ -10,14 +10,28 @@ conexion = pymysql.connect(
 try:
     with conexion.cursor() as cursor:
         #creamos consulta
-        consulta = "SELECT * FROM actor"
+        #consulta = "SELECT * FROM actor"
         #ejecutamos consulta
-        cursor.execute(consulta)
+        #cursor.execute(consulta)
         #obtenemos los datos
-        actores = cursor.fetchall()
+        #actores = cursor.fetchall()
         #recorremos los datos
-        for actor in actores:
-            print(actor)
+        #for actor in actores:
+            #print(actor)
+            # Datos a insertar
+        first_name = 'Jane'
+        last_name = 'Smith'
+
+        # Consulta SQL para insertar un nuevo actor
+        consulta = "INSERT INTO actor (first_name, last_name) VALUES (%s, %s)"
+        
+        # Ejecutamos la consulta pasando los parámetros
+        cursor.execute(consulta, (first_name, last_name))
+
+        # Confirmamos los cambios en la base de datos
+        conexion.commit()
+
+        print("Nuevo actor insertado exitosamente")
 except Exception as e:
     print("Ocurrió un error al consultar: ", e)
             
